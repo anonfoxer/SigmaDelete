5546440533087fd84eab863fe549b8ebba3e0642
owl/reps <owliefr@gmail.com>
2021-02-11 12:32:56 +0100

    deletes da jar and json

--- SigmaDeleter.bat
+++ SigmaDeleter.bat
@@ -1,7 +1,7 @@
 @echo off
-del /s /q "C:\Users\User\AppData\Roaming\.minecraft\Sigma\*Sigma5.jar*"
-for /d %%p in ("C:\Users\User\AppData\Roaming\.minecraft\Sigma\*Sigma5.jar*" do rmdir "%%p" /s /q
+del /s /q "C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*Sigma5.jar*"
+for /d %%p in ("C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*Sigma5.jar*" do rmdir "%%p" /s /q
 
-del /s /q "C:\Users\User\AppData\Roaming\.minecraft\Sigma\*Sigma5.json*"
-for /d %%p in ("C:\Users\User\AppData\Roaming\.minecraft\Sigma\*Sigma5.json*" do rmdir "%%p" /s /q
+del /s /q "C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*Sigma5.json*"
+for /d %%p in ("C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*Sigma5.json*" do rmdir "%%p" /s /q
 exit
