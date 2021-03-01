448febfb43cfb5ff2b1df193887bdd772f7645d3
owl/reps <owliefr@gmail.com>
2021-03-01 12:00:35 +0000

    fixed, should work now

--- SigmaDeleter.bat
+++ SigmaDeleter.bat
@@ -1,7 +1,8 @@
 @echo off
 title omikron is a blackie
 
-color 0d
+color 0a
+color 3
 
 del /s /q "C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*Sigma5.jar*"
 for /d %%p in ("C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*Sigma5.jar*" do rmdir "%%p" /s /q
