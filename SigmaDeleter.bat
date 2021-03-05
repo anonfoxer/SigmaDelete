fb0dbbd405bf5d767957bd1b8626ec23fbc90d5f
Magic <kevinandlife@gmail.com>
2021-03-04 18:23:16 -0600

    Update SigmaDeleter.bat

--- SigmaDeleter.bat
+++ SigmaDeleter.bat
@@ -4,5 +4,6 @@ title Sigma5 Deleter
 del /f /s /q "C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*.*"
 del /f /s /q "C:\Users\%userprofile%\AppData\Roaming\.minecraft\versions\Sigma5\*.*"
 
-echo Sigma5 has been successfully deleted.
+echo Sigma5 has been successfully deleted. you may manually delete the directories or leave them.
+pause
 exit
