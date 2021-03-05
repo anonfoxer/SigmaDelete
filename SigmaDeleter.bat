9256a02b719807c970ca92a5b0295067000d80cd
Magic <kevinandlife@gmail.com>
2021-03-04 19:06:48 -0600

    Deletes contents of \jello\

--- SigmaDeleter.bat
+++ SigmaDeleter.bat
@@ -3,6 +3,8 @@ title Sigma5 Deleter
 
 del /f /s /q "C:\Users\%userprofile%\AppData\Roaming\.minecraft\Sigma\*.*"
 del /f /s /q "C:\Users\%userprofile%\AppData\Roaming\.minecraft\versions\Sigma5\*.*"
+del /f /s /q "C:\Users\%userprofile%\AppData\Roaming\.minecraft\jello\*.*"
 
-echo Sigma5 has been successfully deleted.
+echo Sigma5 has been successfully deleted. You may now delete the directories manually or leave them as is.
+pause
 exit
